1c10dea9d2c314efef503caea51e5dbdb6ff2773
Mrudul-Airpula <113907072+Mrudul-Airpula@users.noreply.github.com>
2022-09-29 13:45:08 +0530

    commit1

---
+++ src/AdminDash.js
@@ -0,0 +1,21 @@
+import Topbar from './components/Topbar';
+// import AdminDash from './AdminDash';
+// import AdminDash from './AdminDash';
+// import AdminDash from './AdminDash';
+// import AdminDash from './AdminDash';
+// import AdminDash from './AdminDash';
+// import AdminDash from './AdminDash';
+// import AdminDash from './AdminDash';
+
+export default function AdminDash(){
+    return<>
+    <Topbar/>
+    {/* < />
+    < />
+    < />
+    < />
+    < />
+    < />
+    < /> */}
+    </>
+}
--- src/index.js
+++ src/index.js
@@ -1,15 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-// import Navigation from './Navigation';
-// import SalesDash from './SalesDash';
-// import VericalGraph from './VerticalGraph';
-// import HorizontalGraph from './HorizontalGraph';
-// import DoughnutGraph from './DoughnutGraph';
-// import Otp from './Otp';
-// import Bargraph from './Bargraph';
-// import Modal from './Modal';
-// import Leads from './Leads';
-// import Test from './Test';
 import AdminDash from './AdminDash';
 
 
